42d92f7b240b1c4b975ebb742268ed7ddf8f62df
LucasLottiPzm <lucas.lotti@grupopzm.com>
2022-03-20 16:03:53 -0300

    making navbar

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,6 +1,25 @@
+import {NavbarDiv, DivForButton, ButtonForInformation, DivForImgAndName, ImgMe, H1Title} from'./NavbarStyled'
+import foto from   './perfil.JPG'
+
 function Navbar() {
     return (
-        <navbar>começando navbar</navbar>
+        <NavbarDiv>
+            <DivForImgAndName>
+                <ImgMe src={foto}></ImgMe>
+                <H1Title>PAlucas</H1Title>
+            </DivForImgAndName>
+            <DivForButton>
+                <ButtonForInformation>
+                    About me/Sobre mim
+                </ButtonForInformation>
+                <ButtonForInformation>
+                    Projects/Projetos
+                </ButtonForInformation>
+                <ButtonForInformation>
+                    Estudos/Studies
+                </ButtonForInformation>
+            </DivForButton>
+        </NavbarDiv>
     );
 }
 
---
+++ src/components/Navbar/NavbarStyled.js
@@ -0,0 +1,29 @@
+import styled from 'styled-components';
+
+export const NavbarDiv = styled.nav`
+    display:flex;
+    background-color: #0000ff;
+    height: 15vh;
+    
+`;
+
+export const DivForButton = styled.div`
+    display:flex;
+`;
+
+export const DivForImgAndName = styled.div`
+    display:flex;
+`;
+
+export const ImgMe = styled.img`
+    border-radius: 50%;
+    height: 98%;
+`;
+
+export const ButtonForInformation = styled.button`
+    font-family: 'Helvetica Neue';
+`;
+
+export const H1Title = styled.h1`
+    font-family: 'Helvetica Neue';
+`;
