c9bfe67c7b189665868c923942dfe2858de78461
LucasLottiPzm <lucas.lotti@grupopzm.com>
2022-04-07 18:02:52 -0300

    changing navbar

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,5 +1,6 @@
-import {NavbarDiv, DivForButton, ButtonForInformation, DivForImgAndName, ImgMe, H1Title} from'./NavbarStyled'
+import {NavbarDiv, DivForButton, ButtonForInformation, DivForImgAndName, ImgMe, H1Title, ButtonForCell} from'./NavbarStyled'
 import foto from   './perfil.JPG'
+import IconCell from './icon.svg'
 
 function Navbar() {
     return (
@@ -19,6 +20,9 @@ function Navbar() {
                     Estudos/Studies
                 </ButtonForInformation>
             </DivForButton>
+                <ButtonForCell>
+                    <img src={IconCell}></img>
+                </ButtonForCell>
         </NavbarDiv>
     );
 }
--- src/components/Navbar/NavbarStyled.js
+++ src/components/Navbar/NavbarStyled.js
@@ -2,26 +2,43 @@ import styled from 'styled-components';
 
 export const NavbarDiv = styled.nav`
     display:flex;
-    background-color: #0000ff;
-    height: 15vh;
-    
+    justify-content: space-between;
+    background-color: #5968b9;
+    height: 10vh;
 `;
 
 export const DivForButton = styled.div`
     display:flex;
+    align-items:center;
+    margin: 5px 5px;
+    @media (max-width: 564px) {
+        display: none;
+    }
 `;
 
+export const ButtonForCell = styled.div`
+    display:flex;
+    align-items:center;
+    margin: 0 5px;
+    @media (min-width: 564px) {
+        display: none;
+    }
+`;
 export const DivForImgAndName = styled.div`
     display:flex;
+    align-items:center;
+    margin: 0 5px;
 `;
 
 export const ImgMe = styled.img`
     border-radius: 50%;
-    height: 98%;
+    height: 50%;
 `;
 
-export const ButtonForInformation = styled.button`
+export const ButtonForInformation = styled.a`
+    margin: 0 5px;
     font-family: 'Helvetica Neue';
+    height:50%;
 `;
 
 export const H1Title = styled.h1`
